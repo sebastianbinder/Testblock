8ff0ae7c07172218813ca287d1de9e3c213e3be7
Sebastian Binder <designstudio@sebastianbinder.com>
2017-05-04 07:36:53 +0200

    Fix Rotation and Saving Schematic

--- src/main/java/com/sebastianbinder/testblock/TestblockHelper.java
+++ src/main/java/com/sebastianbinder/testblock/TestblockHelper.java
@@ -9,9 +9,12 @@ import com.sk89q.worldedit.Vector;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.bukkit.BukkitUtil;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardWriter;
 import com.sk89q.worldedit.schematic.SchematicFormat;
 import com.sk89q.worldedit.session.ClipboardHolder;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import org.bukkit.ChatColor;
@@ -19,104 +22,6 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 class TestblockHelper {
-
-  static void pasteTestblock(Player player) {
-    if (TestblockPlugin.getInstance().isWorldEditEnabled()) {
-      Location location = TestblockPlugin.getInstance().getTestblockLocation(player);
-      if (location == null) {
-        TestblockPlugin.getInstance()
-            .sendPluginMessage(player, ChatColor.GOLD
-                + "Bitte setze den Ort des Testblocks mit \"/testblock setlocation\"!");
-        return;
-      }
-      try {
-        File directory = getSchematicDirectory();
-
-        String testblockName = TestblockPlugin.getInstance().getTestblockSelection(player);
-        if (testblockName == null) {
-          testblockName = "default";
-        }
-
-        File schematicFile = new File(directory,
-            player.getUniqueId().toString() + "-" + testblockName + ".schematic");
-        if (!schematicFile.exists()) {
-          schematicFile = new File(directory, "testblock.schematic");
-        }
-        if (schematicFile.exists()) {
-          EditSession editSession = TestblockPlugin.getInstance().getTestblockEditSession(player);
-          editSession.enableQueue();
-
-          CuboidClipboard clipboard = SchematicFormat.MCEDIT.load(schematicFile);
-
-          org.bukkit.util.Vector bukkitVector = new org.bukkit.util.Vector(
-              clipboard.getOffset().getX(), clipboard.getOffset().getY(),
-              clipboard.getOffset().getZ());
-
-          Direction testblockDirection = Direction.getDirectionFromVector(bukkitVector);
-          Direction pasteDirection = Direction.getDirectionFromVector(location.getDirection());
-
-          clipboard.rotate2D(Direction.getOffset(pasteDirection, testblockDirection));
-          clipboard.paste(editSession, BukkitUtil.toVector(location), false);
-          editSession.flushQueue();
-        }
-      } catch (Exception exception) {
-        exception.printStackTrace();
-      }
-    }
-  }
-
-  static void saveTestblock(Player player, String name) {
-    if (TestblockPlugin.getInstance().isWorldEditEnabled()) {
-      try {
-        WorldEditPlugin worldEditPlugin = TestblockPlugin.getInstance().getWorldEditPlugin();
-        WorldEdit worldEdit = TestblockPlugin.getInstance().getWorldEdit();
-
-        File directory = getSchematicDirectory();
-
-        if (name == null) {
-          name = "default";
-        }
-        File schematicFile = new File(directory, player.getUniqueId().toString() + "-" +
-            name + ".schematic");
-
-        LocalPlayer localPlayer = worldEditPlugin.wrapPlayer(player);
-
-        LocalSession localSession = worldEdit.getSession(localPlayer);
-        ClipboardHolder clipboardSelection = null;
-        try {
-          clipboardSelection = localSession.getClipboard();
-        } catch (Exception ignored) {
-        }
-
-        if (clipboardSelection == null) {
-          TestblockPlugin.getInstance()
-              .sendPluginMessage(player,
-                  ChatColor.RED + "Bitte wähle einen Bereich aus und kopiere ihn mit \"//copy\".");
-          return;
-        }
-        Vector min = clipboardSelection.getClipboard().getMinimumPoint();
-        Vector max = clipboardSelection.getClipboard().getMaximumPoint();
-        Vector origin = clipboardSelection.getClipboard().getOrigin();
-
-        EditSession editSession = TestblockPlugin.getInstance().getTestblockEditSession(player);
-
-        editSession.enableQueue();
-        CuboidClipboard clipboard = new CuboidClipboard(
-            max.subtract(min).add(new Vector(1, 1, 1)),
-            min, min.subtract(origin));
-
-        clipboard.copy(editSession);
-        SchematicFormat.MCEDIT.save(clipboard, schematicFile);
-        editSession.flushQueue();
-
-        TestblockPlugin.getInstance()
-            .sendPluginMessage(player,
-                ChatColor.GREEN + "Dein Testblock wurde erfolgreich gespeichert.");
-      } catch (Exception exception) {
-        exception.printStackTrace();
-      }
-    }
-  }
 
   public static List<String> getPlayerTestblockNames(Player player) {
     File directory = getSchematicDirectory();
@@ -149,6 +54,99 @@ class TestblockHelper {
       directory.mkdirs();
     }
     return directory;
+  }
+
+  static void pasteTestblock(Player player) {
+    if (TestblockPlugin.getInstance().isWorldEditEnabled()) {
+      Location location = TestblockPlugin.getInstance().getTestblockLocation(player);
+      if (location == null) {
+        TestblockPlugin.getInstance()
+            .sendPluginMessage(player, ChatColor.GOLD
+                + "Bitte setze den Ort des Testblocks mit \"/testblock setlocation\"!");
+        return;
+      }
+      try {
+        File directory = getSchematicDirectory();
+
+        String testblockName = TestblockPlugin.getInstance().getTestblockSelection(player);
+        if (testblockName == null) {
+          testblockName = "default";
+        }
+
+        File schematicFile = new File(directory,
+            player.getUniqueId().toString() + "-" + testblockName + ".schematic");
+        if (!schematicFile.exists()) {
+          schematicFile = new File(directory, "testblock.schematic");
+        }
+        if (schematicFile.exists()) {
+          EditSession editSession = TestblockPlugin.getInstance().getTestblockEditSession(player);
+          editSession.enableQueue();
+
+          CuboidClipboard clipboard = SchematicFormat.MCEDIT.load(schematicFile);
+
+          Vector widthVector = clipboard.getOffset().add(clipboard.getWidth(), 0, 0);
+          Vector lengthVector = clipboard.getOffset().add(0, 0, clipboard.getLength());
+          Vector middleVector = Vector.getMidpoint(widthVector, lengthVector);
+          org.bukkit.util.Vector bukkitVector = new org.bukkit.util.Vector(
+              middleVector.getX(), middleVector.getY(),
+              middleVector.getZ());
+
+          Direction testblockDirection = Direction.getDirectionFromVector(bukkitVector);
+          Direction pasteDirection = Direction.getDirectionFromVector(location.getDirection());
+
+          clipboard.rotate2D(Direction.getOffset(pasteDirection, testblockDirection));
+          clipboard.paste(editSession, BukkitUtil.toVector(location), false);
+          editSession.flushQueue();
+        }
+      } catch (Exception exception) {
+        exception.printStackTrace();
+      }
+    }
+  }
+
+  static void saveTestblock(Player player, String name) {
+    if (TestblockPlugin.getInstance().isWorldEditEnabled()) {
+      try {
+        WorldEditPlugin worldEditPlugin = TestblockPlugin.getInstance().getWorldEditPlugin();
+        WorldEdit worldEdit = TestblockPlugin.getInstance().getWorldEdit();
+
+        File directory = getSchematicDirectory();
+
+        if (name == null) {
+          name = "default";
+        }
+        File schematicFile = new File(directory, player.getUniqueId().toString() + "-" +
+            name + ".schematic");
+
+        LocalPlayer localPlayer = worldEditPlugin.wrapPlayer(player);
+
+        LocalSession localSession = worldEdit.getSession(localPlayer);
+
+        ClipboardHolder clipboardSelection = null;
+        try {
+          clipboardSelection = localSession.getClipboard();
+        } catch (Exception ignored) {
+        }
+
+        if (clipboardSelection == null) {
+          TestblockPlugin.getInstance()
+              .sendPluginMessage(player,
+                  ChatColor.RED + "Bitte wähle einen Bereich aus und kopiere ihn mit \"//copy\".");
+          return;
+        }
+
+        FileOutputStream stream = new FileOutputStream(schematicFile);
+        ClipboardWriter writer = ClipboardFormat.SCHEMATIC.getWriter(stream);
+        writer.write(clipboardSelection.getClipboard(), null);
+        writer.close();
+
+        TestblockPlugin.getInstance()
+            .sendPluginMessage(player,
+                ChatColor.GREEN + "Dein Testblock wurde erfolgreich gespeichert.");
+      } catch (Exception exception) {
+        exception.printStackTrace();
+      }
+    }
   }
 
 }
